36a9896dea24c69355a4acb3a8d59dc25917a5b4
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-06-02 14:53:38 -0400

    filtering basic level entries

--- subregions.py
+++ subregions.py
@@ -4,6 +4,30 @@ import re
 
 rank_regx = re.compile("(\d+)( of )(\d+)")
 
+class Subregions(object):
+    def __init__(self):
+        self.subregions = []
+
+    def for_file(self, filename):
+        """
+        Returns a list of subregion objests for the specified file
+        :return: subregions
+        """
+        regions = []
+        for subregion in self.subregions:
+            if subregion.filename == filename:
+                regions.append(subregion)
+
+        return regions
+
+    def add(self, subregion):
+        """
+        Adds a given subregion to the collection of all subregions
+        :param subregion: a Subregion object
+        """
+        self.subregions.append(subregion)
+
+
 class Subregion(object):
     def __init__(self, filename, rank, onset):
         self.filename = filename
@@ -23,7 +47,7 @@ class Subregion(object):
 
 
 def get_subregions(all_comments):
-    subregions = []
+    subregions = Subregions()
     curr_subregion = None
     content = ""
     for index, comment in enumerate(all_comments):
@@ -32,10 +56,7 @@ def get_subregions(all_comments):
             if "starts" in content:
                 rank_reg_result = rank_regx.search(content)
                 rank = None
-                onset_split = comment[2].split("_")
-                print onset_split
-                onset = onset_split[1]
-                print index
+                onset = comment[2].split("_")[1]
                 if rank_reg_result:
                     rank = rank_reg_result.group(1)
 
@@ -44,15 +65,25 @@ def get_subregions(all_comments):
             if "ends" in content:
                 offset = comment[2].split("_")[1]
                 curr_subregion.offset = offset
-                subregions.append(curr_subregion)
+                subregions.add(curr_subregion)
 
     return subregions
+
+def basic_level_file_partition(all_basic_level, filename):
+    bl_for_file = []
+
+    for entry in all_basic_level:
+        if entry[0][0:5] == filename[0:5]:
+            bl_for_file.append(entry)
+
+    return bl_for_file
 
 
 if __name__ == "__main__":
 
-    comments_file = sys.argv[1]
-    #basic_level_file = sys.argv[2]
+    filename = sys.argv[1]
+    comments_file = sys.argv[2]
+    basic_level_file = sys.argv[3]
 
     all_comments = []
     with open(comments_file, "rU") as comments:
@@ -61,13 +92,18 @@ if __name__ == "__main__":
         for comment in reader:
             all_comments.append(comment)
 
-    # all_basic_levels = []
-    # with open(basic_level_file, "rU") as basic_level:
-    #     reader = csv.reader(basic_level)
-    #     reader.next()
-    #     for entry in reader:
-    #         all_basic_levels.append(entry)
+    all_basic_levels = []
+    with open(basic_level_file, "rU") as basic_level:
+        reader = csv.reader(basic_level)
+        reader.next()
+        for entry in reader:
+            all_basic_levels.append(entry)
 
-    subregions = get_subregions(all_comments)
+    all_subregions = get_subregions(all_comments)
+    bl_for_file = basic_level_file_partition(all_basic_levels, filename)
 
-    print subregions
+    print all_subregions
+    print bl_for_file
+
+    subr_for_file = all_subregions.for_file(filename)
+    print subr_for_file
