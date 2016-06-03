8a37275514c966922daa4869a01581ca0ff21c4b
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-06-03 12:03:44 -0400

    switch to indexing subregions by rank

--- subregions.py
+++ subregions.py
@@ -4,70 +4,125 @@ import re
 
 rank_regx = re.compile("(\d+)( of )(\d+)")
 
-class Subregions(object):
+class AllFiles(object):
     def __init__(self):
-        self.subregions = []
+        self.files = {}
 
-    def for_file(self, filename):
-        """
-        Returns a list of subregion objests for the specified file
-        :return: subregions
-        """
-        regions = []
-        for subregion in self.subregions:
-            if subregion.filename == filename:
-                regions.append(subregion)
 
-        return regions
+    def add_file(self, subregion_group):
+        self.files[subregion_group.file] = subregion_group
+
+    def get_file(self, file):
+        if file not in self.files:
+            return None
+        else:
+            return self.files[file]
+
+
+class SubregionGroup(object):
+    def __init__(self):
+        """
+        self.subregions is a dictionary mapping subregion ranks
+        to subregion objects. Each SubregionGroup represents a single
+        CLAN file
+        """
+        self.file = ""
+
+        self.subregions = {}
+        self.unbounded_words = []
+
+
+    def get_subregion(self, region_rank):
+        """
+        Returns a subregion objects for the specified rank
+        :return: subregion
+        """
+        if region_rank not in self.subregions:
+            return None
+        else:
+            return self.subregions[region_rank]
+
 
     def add(self, subregion):
         """
         Adds a given subregion to the collection of all subregions
         :param subregion: a Subregion object
         """
-        self.subregions.append(subregion)
+        self.subregions[subregion.rank] = subregion
 
+        self.file = subregion.filename[0:5]
+
+    def fill_with_basic_levels(self, basic_levels):
+
+        segments = []
+        for subregion in self.subregions:
+
+
+        for bl in basic_levels:
+            if bl[0][0:5] not in self.subregions:
+                continue
+            else:
+                regions = self.subregions[bl[0:5]]
+                for region in regions
 
 class Subregion(object):
-    def __init__(self, filename, rank, onset):
+    def __init__(self, filename, rank, onset, line):
         self.filename = filename
         self.rank = rank
         self.onset = onset
         self.offset = None
+        self.line = line
 
         self.word_count = 0
 
         self.words = []
 
     def __repr__(self):
-        return "\n\n{} [ \n\trank: {} \n\tonset: {} \n\toffset: {}\n]".format(self.filename,
-                                                                 self.rank,
-                                                                 self.onset,
-                                                                 self.offset)
+        return "\n\n{} [ \n\trank: {} \n\tonset: {} \n\toffset: {}\n]"\
+            .format(self.filename,
+                    self.rank,
+                    self.onset,
+                    self.offset)
 
+    def add_word(self, word):
+        """
+
+        :param word: basic_level entry
+        """
+        self.words.append(word)
 
 def get_subregions(all_comments):
-    subregions = Subregions()
+    subregions = SubregionGroup()
+    all_files = AllFiles()
     curr_subregion = None
+    curr_file = ""
     content = ""
+    first_subr = True
     for index, comment in enumerate(all_comments):
         content = comment[3]
         if "subregion" in content:
             if "starts" in content:
+                if (curr_file != comment[0]) and not first_subr:
+                    all_files.add_file(subregions)
+                    subregions = SubregionGroup()
+
+                curr_file = comment[0]
                 rank_reg_result = rank_regx.search(content)
                 rank = None
                 onset = comment[2].split("_")[1]
+                line = comment[1]
                 if rank_reg_result:
                     rank = rank_reg_result.group(1)
 
-                curr_subregion = Subregion(comment[0], rank, onset)
+                curr_subregion = Subregion(comment[0], rank, onset, line)
                 print curr_subregion
             if "ends" in content:
                 offset = comment[2].split("_")[1]
                 curr_subregion.offset = offset
                 subregions.add(curr_subregion)
+                first_subr = False
 
-    return subregions
+    return all_files
 
 def basic_level_file_partition(all_basic_level, filename):
     bl_for_file = []
@@ -81,7 +136,7 @@ def basic_level_file_partition(all_basic_level, filename):
 
 if __name__ == "__main__":
 
-    filename = sys.argv[1]
+    filename = sys.argv[1][0:5]
     comments_file = sys.argv[2]
     basic_level_file = sys.argv[3]
 
@@ -105,5 +160,11 @@ if __name__ == "__main__":
     print all_subregions
     print bl_for_file
 
-    subr_for_file = all_subregions.for_file(filename)
+    subr_for_file = all_subregions.get_file(filename)
+
+    subr_for_file.fill_with_basic_levels(bl_for_file)
+
     print subr_for_file
+
+    #subr_for_file = all_subregions.for_file(filename)
+    #print subr_for_file
